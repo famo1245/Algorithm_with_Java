efb459b0a42568920b0507b2219f3f4c8f4c3c30
JIYOUNG KIM <kandallee38@gmail.com>
2024-08-12 00:23:55 +0900

    ADD: Solving BOJ 3151
    solving boj 3151

---
+++ algorithm_study/src/unsolved/BOJ3151.java
@@ -0,0 +1,57 @@
+package unsolved;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+
+public class BOJ3151 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        String[] temp = br.readLine().split(" ");
+        int[] students = new int[N];
+        Map<Integer, Integer> counts = new HashMap<>();
+
+        for (int i = 0; i < N; i++) {
+            students[i] = Integer.parseInt(temp[i]);
+            int count = counts.getOrDefault(students[i], 0) + 1;
+            counts.put(students[i], count);
+        }
+
+        Arrays.sort(students);
+        int answer = 0;
+        for (int i = 0; i < N - 1; i++) {
+            int student = students[i];
+            int start = i + 1;
+            int end = N - 1;
+
+            while (start < end) {
+                int sum = students[start] + students[end] + student;
+                if (sum == 0) {
+                    if (students[start] == students[end]) {
+                        answer += end - start;
+                    } else {
+                        // TODO: Count 부분 해결
+                        answer += counts.get(students[end]);
+                    }
+                    start++;
+                } else if (sum < 0) {
+                    start++;
+                } else {
+                    end--;
+                }
+            }
+        }
+
+        System.out.println(answer);
+    }
+}
+
+/*
+ * 1. 중복에 대한 고려 x => 합이 0이 되는 경우의 수를 한 번만 확인하고 넘어감
+ * 2. 더하는 값이 똑같은 경우 고려 x => 무작정 곱하기만 함
+ *
+ * */
