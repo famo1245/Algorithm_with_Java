8a8963495bc05040453d3d9fadc1d999591f8d7d
JIYOUNG KIM <kandallee38@gmail.com>
2024-08-15 23:11:52 +0900

    REFACTOR: Removed unused var
    Removed unused variable

--- algorithm_study/src/BOJ11659V1.java
+++ algorithm_study/src/BOJ11659V1.java
@@ -11,19 +11,18 @@ public class BOJ11659V1 {
 
         int N = Integer.parseInt(st.nextToken());
         int M = Integer.parseInt(st.nextToken());
-
-        int[] nums = new int[N];
+        
         int[] sum = new int[N + 1];
         sum[0] = 0;
 
         // 누적합을 구하기
         st = new StringTokenizer(br.readLine());
         for (int i = 0; i < N; i++) {
-            nums[i] = Integer.parseInt(st.nextToken());
+            int num = Integer.parseInt(st.nextToken());
             if (i == 0) {
-                sum[i + 1] = nums[i];
+                sum[i + 1] = num;
             } else {
-                sum[i + 1] = nums[i] + sum[i];
+                sum[i + 1] = num + sum[i];
             }
 
         }
