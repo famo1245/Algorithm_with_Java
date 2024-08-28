f4e2094a9703c44b396a4b034d8fd4ab461f7580
JIYOUNG KIM <kandallee38@gmail.com>
2024-08-28 22:47:06 +0900

    ADD: Solving BOJ 31406
    Solving BOJ 31406

--- algorithm_study/src/BOJ31406.java
+++ algorithm_study/src/BOJ31406.java
@@ -16,62 +16,81 @@ public class BOJ31406 {
             sys[i] = new ArrayList<>();
         }
 
-        // init
+        // 초기화
         for (int i = 1; i <= N; i++) {
             st = new StringTokenizer(br.readLine());
             int childCount = Integer.parseInt(st.nextToken());
 
             for (int j = 0; j < childCount; j++) {
                 int child = Integer.parseInt(st.nextToken());
-                sys[i].add(new Directory(child));
+                sys[i].add(new Directory(child, i));
             }
         }
 
-        int index = 0;
-        int nodeIndex = 1;
-        int parentNode = 1;
-        Directory current = sys[nodeIndex].get(index);
+        int currentNode = 1;
+        int currentIndex = 0;
+
         for (int i = 0; i < Q; i++) {
             st = new StringTokenizer(br.readLine());
             String command = st.nextToken();
+
             if (command.equals("toggle")) {
-                current.open = !current.open;
-            } else {
+                sys[currentNode].get(currentIndex).toggle();
+            } else if (command.equals("move")) {
                 int count = Integer.parseInt(st.nextToken());
-                while (count > 0) {
-                    if (current.open) {
-                        nodeIndex = current.directoryNum;
-                        index = 0;
-                    } else {
-                        if (index < sys[nodeIndex].size() - 2) {
-                            current = sys[nodeIndex].get(++index);
+                if (count > 0) {
+                    for (int j = 0; j < count; j++) {
+                        Directory current = sys[currentNode].get(currentIndex);
+                        if (current.open) {
+                            currentNode = current.directoryNum;
+                            currentIndex = 0;
                         } else {
-                            count = 0;
+                            if (currentIndex < sys[currentNode].size() - 1) {
+                                currentIndex++;
+                            } else {
+                                break;
+                            }
                         }
                     }
-                    count--;
+                } else {
+                    for (int j = 0; j > count; j--) {
+                        if (currentNode == 1) {
+                            break;  // 루트에서는 더 이상 상위로 이동 불가
+                        }
+                        int parentNode = sys[currentNode].get(0).parent;
+                        currentIndex = getIndexInParent(sys[parentNode], currentNode);
+                        currentNode = parentNode;
+                    }
                 }
-
-                sb.append(current.directoryNum).append('\n');
+                sb.append(sys[currentNode].get(currentIndex).directoryNum).append('\n');
             }
         }
 
         System.out.print(sb);
     }
 
+    static int getIndexInParent(List<Directory> parentDir, int childNum) {
+        for (int i = 0; i < parentDir.size(); i++) {
+            if (parentDir.get(i).directoryNum == childNum) {
+                return i;
+            }
+        }
+        return -1;
+    }
+
     static class Directory {
         int directoryNum;
+        int parent;
         boolean open;
 
-        public Directory(int directoryNum) {
+        public Directory(int directoryNum, int parent) {
             this.directoryNum = directoryNum;
+            this.parent = parent;
+            this.open = false;
         }
 
-        @Override
-        public String toString() {
-            return "Directory{" +
-                    "directoryNum=" + directoryNum +
-                    '}';
+        public void toggle() {
+            this.open = !this.open;
         }
     }
 }
