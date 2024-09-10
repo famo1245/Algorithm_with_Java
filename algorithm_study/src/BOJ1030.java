0900f03a5b40a1588cd295c13224de0bdf6dd466
JIYOUNG KIM <kandallee38@gmail.com>
2024-09-10 17:55:21 +0900

    ADD: Solving BOJ 1030

---
+++ algorithm_study/src/BOJ1030.java
@@ -0,0 +1,20 @@
+import java.util.*;
+import java.io.*;
+
+public class BOJ1030 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int s = Integer.parseInt(st.nextToken());   // 시간
+        int N = Integer.parseInt(st.nextToken());   // N*N
+        int K = Integer.parseInt(st.nextToken());   // K*K 검정색
+        int r1 = Integer.parseInt(st.nextToken());
+        int r2 = Integer.parseInt(st.nextToken());
+        int c1 = Integer.parseInt(st.nextToken());
+        int c2 = Integer.parseInt(st.nextToken());
+
+
+    }
+}
