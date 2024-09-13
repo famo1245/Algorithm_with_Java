20940ccc23a5391544aea2972ce2c9d41d4eabb6
JIYOUNG KIM <kandallee38@gmail.com>
2024-09-13 23:39:50 +0900

    ADD: Solving BOJ 5639

--- algorithm_study/src/BOJ5639.java
+++ algorithm_study/src/BOJ5639.java
@@ -1,29 +1,78 @@
-import java.util.*;
 import java.io.*;
 
 public class BOJ5639 {
 
+    static StringBuilder sb = new StringBuilder();
+    static Node current;
+
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringBuilder sb = new StringBuilder();
         String input = br.readLine();
-        int root = Integer.parseInt(input);
-        Queue<Integer> que = new ArrayDeque<>();
+        Node root = new Node(Integer.parseInt(input));
+        current = root;
 
         while ((input = br.readLine()) != null) {
-            que.add(Integer.parseInt(input));
+            Node node = new Node(Integer.parseInt(input));
+            add(node);
         }
 
-
+        postOrder(root);
+        System.out.print(sb);
     }
 
-    static void postOrder() {
+    static void add(Node node) {
+        System.out.println(current.key + " " + node.key);
+        if (current.key > node.key) {
+            if (current.left == null) {
+                current.left = node;
+                node.parent = current;
+                current = node;
+            } else {
+                current = current.left;
+                add(node);
+            }
+        } else {
+            if (current.parent != null && current.parent.key < node.key) {
+                current = current.parent;
+                add(node);
+                return;
+            }
 
+            if (current.right == null) {
+                current.right = node;
+                node.parent = current;
+                current = node;
+            } else {
+                System.out.println("right: " + current.key + " " + current.right.key);
+                current = current.right;
+                add(node);
+            }
+        }
+    }
+
+    static void postOrder(Node node) {
+        if (node.left != null) {
+            postOrder(node.left);
+        }
+
+        if (node.right != null) {
+            postOrder(node.right);
+        }
+
+        sb.append(node.key).append('\n');
     }
 
     static class Node {
-        int value;
+        int key;
         Node left;
         Node right;
+        Node parent;
+
+        public Node(int key) {
+            this.key = key;
+            this.left = null;
+            this.right = null;
+            this.parent = null;
+        }
     }
 }
