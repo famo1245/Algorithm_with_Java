9035ce583e8211f4f46680fbf8ccc4a3d4a1bdb6
JIYOUNG KIM <kandallee38@gmail.com>
2024-11-05 00:52:36 +0900

    ADD: Solving BOJ 20128

---
+++ algorithm_study/src/BOJ20128.java
@@ -0,0 +1,15 @@
+import java.io.*;
+import java.util.*;
+
+public class BOJ20128 {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
+
+        int[][] distance = new int[N + 1][2];
+    }
+}
